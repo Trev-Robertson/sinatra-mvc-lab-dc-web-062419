83b831f80876d347ffd7f0069d031d26e727391e
Trevahr Robertson <Trevahr@gmail.com>
2019-07-15 20:17:36 -0400

    last question

---
+++ models/piglatinizer.rb
@@ -0,0 +1,31 @@
+class PigLatinizer
+    
+    attr_reader :string
+
+    def initialize(string)
+        @string = string.downcase
+    end
+
+      def latinize
+        string = string.downcase
+        split_string = string.split(" ")
+
+       split_string_more = split_string.map do |word|
+                    new_elem = word.split("")
+            first_letter = new_elem.shift
+            if first_letter =~ /[aeiou]/
+                new_elem << [first_letter, "w", "a", "y"]
+            else
+                new_elem << [first_letter, "a", "y"]
+            end
+            
+       end
+   join_elements = split_string_more.map do |word|
+        word.join
+      end
+      join_elements.join(" ")
+
+      end
+
+
+end
